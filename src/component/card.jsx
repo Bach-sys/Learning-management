852fa78e0a76bde4960e4c40a7d9a28d7521c04a
Bach-sys <bachloppho1202@gmail.com>
2025-08-18 22:03:38 +0700

    this is fking first code

---
+++ src/component/card.jsx
@@ -0,0 +1,15 @@
+//import { Color } from 'antd/es/color-picker'
+import React from 'react'
+
+const Card = () => {
+    const style ={
+        color: 'blue',
+        fontSize: '100 px',
+    }
+
+  return (
+    <div style={style}>Hello world this is my first website make by react js glories to my self that i have touch a bit code and now i will cook this project gg, but first good night</div>
+  )
+}
+
+export default Card
